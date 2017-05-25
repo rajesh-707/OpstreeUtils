10fab8fc93b7f0de8fa759de55b37ac39f5a91eb
rajiv2205 <rajivsharma.2205@gmail.com>
2017-05-25 08:08:33 +0000

    updated key as variables

--- screencapture/printscreen.py
+++ screencapture/printscreen.py
@@ -3,34 +3,47 @@ from PIL import ImageGrab
 from docx import Document
 from docx.shared import Inches
 import sys
-from Xlib.display import Display
+#from Xlib.display import Display
 
 document = Document()
 counter = 0
 filename_prefix = 'demo'
-
+directory = 'C:'
+docx_prefix = sys.argv[1]
+docx_counter = 1
+new_file_key = Key.shift
+screenshot_key = Key.print_screen
 
 def on_press(key):
-    if key == Key.space:
+    if key == screenshot_key:
        print('{0} pressed'.format(
         key))
+    if key == new_file_key :
+        print('creating new file')
 
 def on_release(key):
-#    counter = 0
+
     global counter
-    if key == Key.space :
+    global docx_counter
+    global document
+    if key == screenshot_key :
        print('{0} release'.format(
         key))
        snapshot = ImageGrab.grab()
        str_counter = str(counter)
-       save_path = 'C:\\python\\' + filename_prefix + str_counter + '.jpg'
+       str_docx_counter = str(docx_counter)
+       save_path = directory + '\\python\\' + filename_prefix + str_counter + '.jpg'
        snapshot.save(save_path)
        p = document.add_paragraph()
        r = p.add_run()
        r.add_picture(save_path, width=Inches(7.0))
        
        counter = counter + 1
-    document.save('C:\\python\\' + sys.argv[1])
+       document.save(directory + '\\python\\' + docx_prefix + '_' + str_docx_counter + '.docx')
+    if key == new_file_key :
+       docx_counter = docx_counter + 1
+       document = Document()
+       
     if key == Key.esc:
         # Stop listener
         return False
